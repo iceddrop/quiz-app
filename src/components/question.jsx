56e379cf99044e6444b535e7ecd7c3bb8406913b
iceddrop <93996532+iceddrop@users.noreply.github.com>
2022-08-18 13:34:45 +0100

    first deployment

--- src/components/question.jsx
+++ src/components/question.jsx
@@ -3,25 +3,55 @@ import Questions from './questions'
 import {nanoid} from 'nanoid'
 export default function Question(){
     const [quizData, setQuizData] = React.useState([])
-    console.log(quizData)
+    const [score, setScore] = React.useState(0)
+    const [finishGame, setFinishGame] = React.useState(false)
+    const [refreshData, setRefreshData] = React.useState(false);
 
+    const updateScore = newScore => {
+        setScore(prevScore => prevScore + newScore)
+    }
     React.useEffect(()=> {
         fetch("https://opentdb.com/api.php?amount=5&category=31&type=boolean")
             .then(res => res.json())
             .then(data => setQuizData(quiz =>{
                 return data.results.map(dataObj =>{
-                    return {...dataObj, id:nanoid()}
+                    return {...dataObj, isPicked: false, id:nanoid()}
                 })
                }))
   
     }, [])
-    const quizEl = quizData.map(quiz => (
-    <Questions   quizInfo={quiz}/>
-    ))
+        
+    function pickAnswer(id){
+        setQuizData(val => val.map(item => (
+        item.id === id ?  {...item, isPicked: !item.isPicked} : {...item}
+        ) 
+        ))
+        console.log(id)
+        }
 
+    console.log(score)
+    const quizEl = quizData.map(quiz => (
+    <Questions toggle={()=> pickAnswer(quiz.id)} isPicked={quiz.isPicked} updateScore={updateScore}  quizInfo={quiz}/>
+    ))
+    function submit(){
+        setFinishGame(val =>!val)
+        setRefreshData(refreshData => !refreshData)
+     }
+    function newGame(){
+        if(refreshData === true){
+            window.location.reload();
+        }
+    }
+   
+console.log(quizData)
+ 
     return(
         <>
            {quizEl}
+           <div className="btn-div">
+             { finishGame ?  <button onClick={newGame} className="button-1">New Game</button> : <button onClick={submit} className="button-1">Submit</button>}
+           </div>
+             {finishGame ? <p className='score'>{score}/5</p> : ''} 
         </>
     )
 }
--- src/components/questions.jsx
+++ src/components/questions.jsx
@@ -3,26 +3,22 @@ import React from 'react'
 export default function Questions(props){
     const answer1 = props.quizInfo.correct_answer
     const answer2 = props.quizInfo.incorrect_answers[0]
-    const [score, setScore] = React.useState(0)
-    console.log(answer1)
-    console.log(answer2)
+      
   const handleClick = (isCorrect) => {
        if(isCorrect == answer1){
-        return setScore(prevScore => prevScore + 1)
+        return props.updateScore(1)
        }else{
         console.log('wrong')
        }
   }
-
   
-  console.log(score)
+
     return(
         <div className='quiz-div'>
           <h4 className="question">{props.quizInfo.question}</h4>
           <div className='options-list'>
-            <button onClick={()=>{handleClick(answer1)}} className="option">{answer1}</button>
-            <button onClick={()=>{handleClick(answer2)}}  className="option">{answer2}</button>
-
+            <button onClick={()=>{handleClick(answer1)}} className="dormant-button">{answer1}</button>
+            <button onClick={()=>{handleClick(answer2)}}  className="dormant-button ml">{answer2}</button>
           </div>
         </div>
     )
